e0be97a511a51ab811eef2010ff5ae6f091cd2a4
samuelroman <sami5998@gmail.com>
2019-02-28 15:52:35 +0100

    Estado inicial con el factorial iterativo implementado en Operacions

---
+++ src/operacions/OperacionsTest.java
@@ -0,0 +1,30 @@
+package operacions;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OperacionsTest {
+	Operacions fn;
+
+	@Before
+	public void crearClasse() {
+		fn = new Operacions();
+	}
+
+	@Test
+	public void getRecursivaTest() {
+		Assert.assertEquals(120, fn.getRecursiva(5), 0.001);
+	}
+
+	@Test
+	public void getIterativaTest() {
+		Assert.assertEquals(120, fn.getIterativa(5), 0.001);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void getExceptionTest() {
+		double valor = fn.getIterativa(-3);
+		System.out.println(valor);
+	}
+}
